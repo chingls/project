e7f255869fa3b4f847eacf36ca9fbb9f66c1cad6
Chingis <flamingmanss@gmail.com>
2020-10-31 23:21:21 +0500

    Домашнее задание

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('насколько вы его оцениаете', ''),
+      c = prompt('Один из последних просмотренных фильмов',''),
+      d = prompt('насколько вы его оцениаете', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
